7875ae7292e62c56cdd14ca74977e59f9f6f6aa1
ruhaniia <ruhaniia30@gmail.com>
2025-07-04 21:30:36 -0400

    Update AboutPreview.jsx

--- src/components/AboutPreview.jsx
+++ src/components/AboutPreview.jsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import personalInfo from "../data/staticContent"; // adjust the path as needed
+import personalInfo from "../data/staticContent"; 
 
 export default function AboutPreview() {
   return (
